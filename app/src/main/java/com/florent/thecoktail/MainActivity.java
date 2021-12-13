bfd49b82ed8196b758fc64bda399dd7e982444cf
Deswaerte Florent <pro.flo.contact@gmail.com>
2021-12-13 09:46:44 +0100

    13/12 : Navigation

--- app/src/main/java/com/florent/thecoktail/MainActivity.java
+++ app/src/main/java/com/florent/thecoktail/MainActivity.java
@@ -1,14 +1,58 @@
 package com.florent.thecoktail;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import android.view.MenuItem;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+
+    Fragment RandomFragment;
+    Fragment ListFragment;
+    Fragment SearchFragment;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RandomFragment = new RandomFragment();
+        ListFragment = new ListFragment();
+        SearchFragment = new SearchFragment();
+
+        loadFragment(RandomFragment);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnItemSelectedListener(this);
+    }
+
+    private boolean loadFragment(Fragment fragment){
+        if(fragment != null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView, fragment).commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+        switch(item.getItemId()){
+            case R.id.action_random:
+                fragment = RandomFragment;
+                break;
+            case R.id.action_list:
+                fragment = ListFragment;
+                break;
+            case R.id.action_search:
+                fragment = SearchFragment;
+                break;
+        }
+        return loadFragment(fragment);
     }
 }
